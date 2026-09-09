b2a952ec37f53187759ffc79e4eeb2767d60c664
Link4Electronics <link4electronics@gmail.com>
2026-09-09 04:09:45 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,14 +3,13 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q abaddon | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/uowuo/abaddon/refs/heads/master/res/desktop/icon.svg
 export DESKTOP=https://raw.githubusercontent.com/uowuo/abaddon/refs/heads/master/res/desktop/io.github.uowuo.abaddon.desktop
-export STARTUPWMCLASS=abaddon
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/abaddon /usr/share/abaddon
